a2462e28fd6ec1426e4ca8a59c58f393abf451cb
Mg Chit <chitlunh@gmail.com>
2023-12-02 22:29:23 +0630

    [Complete] Loading

--- BlazorWasm.ArtGallery/BlazorWasm.ArtGallery/wwwroot/theme/js/scripts.js
+++ BlazorWasm.ArtGallery/BlazorWasm.ArtGallery/wwwroot/theme/js/scripts.js
@@ -6,120 +6,125 @@
  * 5. Preload
  */
 
-'use strict';
+//'use strict';
 
-(function ($) {
+//(function ($) {
 
-	$.fn.tanchoMagnificPopup = function (opts) {
+//	$.fn.tanchoMagnificPopup = function (opts) {
 
-		var $self = $(this),
-			options = $.extend({
-				delegate   : '.media',
-				type       : 'image',
-				tLoading   : '<div class="dots">\
-							<div class="dot active"></div>\
-							<div class="dot active"></div>\
-							<div class="dot active"></div>\
-							<div class="dot active"></div>\
-						</div>',
-				mainClass  : 'mfp-img-mobile',
-				gallery    : {
-					enabled           : true,
-					navigateByImgClick: true,
-					preload           : [0, 3] // Will preload 0 - before current, and 1 after the current image
-				},
-				image      : {
-					tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-				},
-				closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
-				callbacks  : {
-					markupParse      : function (item) {
-					},
-					imageLoadComplete: function () {
-						var $container = $('.mfp-container');
+//		var $self = $(this),
+//			options = $.extend({
+//				delegate   : '.media',
+//				type       : 'image',
+//				tLoading   : '<div class="dots">\
+//							<div class="dot active"></div>\
+//							<div class="dot active"></div>\
+//							<div class="dot active"></div>\
+//							<div class="dot active"></div>\
+//						</div>',
+//				mainClass  : 'mfp-img-mobile',
+//				gallery    : {
+//					enabled           : true,
+//					navigateByImgClick: true,
+//					preload           : [0, 3] // Will preload 0 - before current, and 1 after the current image
+//				},
+//				image      : {
+//					tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//				},
+//				closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
+//				callbacks  : {
+//					markupParse      : function (item) {
+//					},
+//					imageLoadComplete: function () {
+//						var $container = $('.mfp-container');
 
-						$container.addClass('load-done');
-						setTimeout(function () {
-							$container.addClass('load-transition');
-						}, 50);
-					},
-					change           : function () {
-						var $container = $('.mfp-container');
-						$container.removeClass('load-done load-transition');
-					}
+//						$container.addClass('load-done');
+//						setTimeout(function () {
+//							$container.addClass('load-transition');
+//						}, 50);
+//					},
+//					change           : function () {
+//						var $container = $('.mfp-container');
+//						$container.removeClass('load-done load-transition');
+//					}
 
-				}
-			}, $self.data(), opts);
-		$('.media', $self).each( function () {
-			var href = $(this).data('url');
+//				}
+//			}, $self.data(), opts);
+//		$('.media', $self).each( function () {
+//			var href = $(this).data('url');
 
-			if (href && href !== '') {
-				$(this).attr('href', href);
-			}
+//			if (href && href !== '') {
+//				$(this).attr('href', href);
+//			}
 
-		});
-		$self.magnificPopup(options);
-	};
+//		});
+//		$self.magnificPopup(options);
+//	};
 
-	$(document).on('ready', function () {
+//	$(document).on('ready', function () {
 
-		var $header = $('.header'),
-			$search = $('.fa-search', $header);
+//		var $header = $('.header'),
+//			$search = $('.fa-search', $header);
 
-		// 1. Search Popup
-		$search.on('click', function (e) {
-			e.preventDefault();
-			$('.box-search').slideToggle(400);
-		});
+//		// 1. Search Popup
+//		$search.on('click', function (e) {
+//			e.preventDefault();
+//			$('.box-search').slideToggle(400);
+//		});
 
 
 
-		// 2. Index Grid
-		var $gridLayout = $('.grid-layout');
-		$gridLayout.imagesLoaded( function () {
-			$gridLayout.masonry({
-				itemSelector: '.post',
-			});
-		});
+//		// 2. Index Grid
+//		var $gridLayout = $('.grid-layout');
+//		$gridLayout.imagesLoaded( function () {
+//			$gridLayout.masonry({
+//				itemSelector: '.post',
+//			});
+//		});
 
-		// 3. Menu Mobile
-		var $btnMenu = $('.menu-mobile'),
-			$hideMenu = $('.hide-menu');
+//		// 3. Menu Mobile
+//		var $btnMenu = $('.menu-mobile'),
+//			$hideMenu = $('.hide-menu');
 
-		$btnMenu.on('click', function () {
-			$header.toggleClass('active');
+//		$btnMenu.on('click', function () {
+//			$header.toggleClass('active');
 
-			if ($header.hasClass('active')) {
-				$hideMenu.addClass('active');
-			}
-			else {
-				$hideMenu.removeClass('active');
-			}
-		});
-		$hideMenu.on('click', function () {
-			$header.removeClass('active');
-			$hideMenu.removeClass('active');
-		});
+//			if ($header.hasClass('active')) {
+//				$hideMenu.addClass('active');
+//			}
+//			else {
+//				$hideMenu.removeClass('active');
+//			}
+//		});
+//		$hideMenu.on('click', function () {
+//			$header.removeClass('active');
+//			$hideMenu.removeClass('active');
+//		});
 
-		$('.menu-item-has-children', '.main-menu').on('click', ' > a', function (e) {
-			var ww = $(window).width();
+//		$('.menu-item-has-children', '.main-menu').on('click', ' > a', function (e) {
+//			var ww = $(window).width();
 
-			if (ww <=991) {
-				var $parent = $(e.target).closest('.menu-item-has-children');
-				e.preventDefault();
-				$('>.sub-menu', $parent).slideToggle(400);
-			}
-		});
+//			if (ww <=991) {
+//				var $parent = $(e.target).closest('.menu-item-has-children');
+//				e.preventDefault();
+//				$('>.sub-menu', $parent).slideToggle(400);
+//			}
+//		});
 
-		// 5. Preload
+//		// 5. Preload
 
-		var $preload = $('#preload');
+//		var $preload = $('#preload');
 
-		if ($preload.length) {
-			$(window).on('load', function () {
-				$preload.fadeOut(400);
-			});
-		}
-	});
+//		if ($preload.length) {
+//			$(window).on('load', function () {
+//				$preload.fadeOut(400);
+//			});
+//		}
+//	});
 
-})(jQuery);
+//})(jQuery);
+
+window.enableLoading = function (enable) {
+	console.log("entered");
+	$('#preload').css('display', enable ? 'flex' : 'none');
+}
